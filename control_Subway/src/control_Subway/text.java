d92d43fc00d1c492f13345a72a910de1f88ec15e
U201717109 <3151727911@qq.com>
2018-10-27 00:22:05 +0800

    完成了两个任任务

--- control_Subway/src/control_Subway/text.java
+++ control_Subway/src/control_Subway/text.java
@@ -1,5 +1,7 @@
 package control_Subway;
 
+import java.util.Scanner;
+
 import control_Subway.NotFoundException;
 import control_Subway.subwayDA;
 
@@ -7,13 +9,33 @@ public class text {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//链接数据库
 		subwayDA.initialize();
+		//给定站定名，返回经过该站点的所有线路的名称集合
 		try {
-			subwayDA.findTarget();
+			System.out.println("请输入想查询的站点名");
+			Scanner in=new Scanner(System.in);
+			String subwayName=in.nextLine();
+			subwayDA.findLine(subwayName);
 		} catch (NotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		//给定线路名和终点方向（比如 2 号线，光谷广场方向），返回 该线路中所有站点的顺序列表
+		try {
+			System.out.println("请给定线路名和终点方向（比如 2 号线，光谷广场方向）");
+			System.out.println("请输入想查询的线路：");
+			Scanner in=new Scanner(System.in);
+			String lineName=in.nextLine();
+			System.out.println("请输入想查询的终点方向：");
+			Scanner in1=new Scanner(System.in);
+			String endStation=in1.nextLine();
+			subwayDA.getStations(lineName, endStation);
+		}catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//关闭数据库
 		subwayDA.terminate();
 	}
 
